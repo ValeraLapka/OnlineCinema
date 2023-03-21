484dad9f52b872712694d0adde3659f8644e7555
Валерия <Valera_lapka@mail.ru>
2023-03-21 20:59:13 +0300

    init

---
+++ src/main/java/ru/edu/bsu/onlinecinema/OnlineCinemaApplication.java
@@ -0,0 +1,13 @@
+package ru.edu.bsu.onlinecinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/edu/bsu/onlinecinema/OnlineCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.edu.bsu.onlinecinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
