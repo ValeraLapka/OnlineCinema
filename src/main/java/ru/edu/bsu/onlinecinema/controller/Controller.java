bace9fd1d07745f58f6f10ead3ef365e84b076dc
Валерия <Valera_lapka@mail.ru>
2023-03-22 21:46:01 +0300

    add index controller

--- src/main/java/ru/edu/bsu/onlinecinema/controller/Controller.java
+++ src/main/java/ru/edu/bsu/onlinecinema/controller/Controller.java
@@ -5,13 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import ru.edu.bsu.onlinecinema.persistance.UserDto;
+import ru.edu.bsu.onlinecinema.persistance.dto.UserDto;
+import ru.edu.bsu.onlinecinema.service.SessionService;
 import ru.edu.bsu.onlinecinema.service.UserService;
 
 @org.springframework.stereotype.Controller
 @RequiredArgsConstructor
 public class Controller {
     private final UserService userService;
+    private final SessionService sessionService;
 
     @GetMapping("/registration")
     public String registration(@ModelAttribute("user") UserDto userDto){
@@ -21,6 +23,11 @@ public class Controller {
     public String registrationProcess(@ModelAttribute("user") UserDto userDto){
         userService.registration(userDto);
         return "registration";
+    }
+    @GetMapping("/")
+    public String index(Model model){
+        model.addAttribute("sessions",sessionService.getAllSession());
+        return "index";
     }
 
 }
--- src/main/webapp/WEB-INF/view/index.jsp
+++ src/main/webapp/WEB-INF/view/index.jsp
@@ -1,9 +1,27 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page import="ru.edu.bsu.onlinecinema.persistance.dto.SessionDto" %>
 <html>
 <head>
     <title>Страница пользователя</title>
 </head>
 <body>
-    <h1>Привет</h1>
+    <h1>Расписание</h1>
+    <table border="1">
+        <caption>Доступные сеансы</caption>
+        <tr>
+            <th>Фильм</th>
+            <th>Время</th>
+            <th>Количество мест</th>
+        </tr>
+        <c:forEach items="${sessions}" var="session" >
+            <tr>
+                <td>${session.filmName}</td>
+                <td>${session.time}</td>
+                <td>${session.placeCount}</td>
+
+            </tr>
+        </c:forEach>
+    </table>
 </body>
 </html>
